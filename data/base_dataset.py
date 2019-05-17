8ed5fd2927baa7f18c4a6038e8231840623b5e6a
davicorreiajr <davicorreiajr@gmail.com>
2019-05-17 11:14:23 +0300

    remove more params from test options

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -60,24 +60,6 @@ class BaseDataset(data.Dataset, ABC):
         pass
 
 
-def get_params(opt, size):
-    w, h = size
-    new_h = h
-    new_w = w
-    if opt.preprocess == 'resize_and_crop':
-        new_h = new_w = opt.load_size
-    elif opt.preprocess == 'scale_width_and_crop':
-        new_w = opt.load_size
-        new_h = opt.load_size * h // w
-
-    x = random.randint(0, np.maximum(0, new_w - opt.crop_size))
-    y = random.randint(0, np.maximum(0, new_h - opt.crop_size))
-
-    flip = random.random() > 0.5
-
-    return {'crop_pos': (x, y), 'flip': flip}
-
-
 def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
     transform_list = []
     if grayscale:
