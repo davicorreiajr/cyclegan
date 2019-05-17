affbe8efa3b2532897d000fde14776cf5c1df9bd
davicorreiajr <davicorreiajr@gmail.com>
2019-05-17 21:26:44 +0300

    Change preprocess to none

--- test.py
+++ test.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
         num_threads=0,#
         output_nc=3,
         phase='test',
-        preprocess='resize_and_crop',#
+        preprocess='none',#
         results_dir='/content/cyclegan/results',
         serial_batches=True,#
         suffix='',
