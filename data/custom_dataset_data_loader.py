9714c1b054248401894d6d1a7dcbd1ad2277e979
davicorreiajr <davicorreiajr@gmail.com>
2019-05-17 11:09:54 +0300

    Remove use of max_dataset_size in CustomDatasetDataLoader

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -31,6 +31,4 @@ class CustomDatasetDataLoader():
     def __iter__(self):
         """Return a batch of data"""
         for i, data in enumerate(self.dataloader):
-            if i * self.opt.batch_size >= self.opt.max_dataset_size:
-                break
             yield data
