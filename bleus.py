fdb3bab531ae672c252855dc871739c043a73a8f
davicorreiajr <davicorreiajr@gmail.com>
2019-05-18 11:01:40 +0300

    Change bleus dummy file

--- bleus.py
+++ bleus.py
@@ -1,11 +1,2 @@
-import torch
-
-current_device = torch.cuda.current_device()
-print('current_device = ', current_device)
-
-count = torch.cuda.device_count()
-print('count = ', count)
-
-for i in range(count):
-    name = torch.cuda.get_device_name(0)
-    print('name = ', name)
+def test():
+    print('HAHA')
