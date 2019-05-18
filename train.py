2764592c391df5e1399a255b2a04121147da7c2a
davicorreiajr <davicorreiajr@gmail.com>
2019-05-18 17:15:20 +0300

    Increase batch_size option to 16 in train

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def run(options_raw):
 
 if __name__ == '__main__':
     options_raw = dict(
-        batch_size=1,
+        batch_size=16,
         beta1=0.5,
         checkpoints_dir='./checkpoints',
         crop_size=256,
